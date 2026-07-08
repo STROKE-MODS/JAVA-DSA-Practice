8a7689c0795c9f58ed2814c7c6d983e271a92ed1
STROKE-MODS <strokemods@gmail.com>
2026-07-09 02:30:18 +0530

    Another commit

---
+++ pattern8.java
@@ -0,0 +1,13 @@
+public class pattern8 {
+    public static void main(String a[]){
+        int number=1;
+        int n=5;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print(number+" ");
+                number++;
+            }
+            System.out.println();
+        }
+    }
+}
