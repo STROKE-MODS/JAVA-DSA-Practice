746c162d6b025333aa8120ee2c1411b728ebb779
STROKE-MODS <strokemods@gmail.com>
2026-07-09 23:49:06 +0530

    Hello world

---
+++ pattern9.java
@@ -0,0 +1,12 @@
+public class pattern9 {
+    public static void main(String a[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+            char hi ='A';
+            for(int k=0;k<=i;k++,hi++){
+                System.out.print(hi+" ");
+            }
+            System.out.println();
+        }
+    }
+}
