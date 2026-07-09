0649e276e091e543db17d50154229a69dd4a5a03
STROKE-MODS <strokemods@gmail.com>
2026-07-09 23:45:21 +0530

    Day 7

---
+++ pattern10.java
@@ -0,0 +1,11 @@
+public class pattern10 {
+    public static void main(String a[]){
+        int n=5;
+        for(int i=0;i<n;i++){
+        for(char hi= 'A';hi<('A'+5-i);hi++){
+            System.out.print(hi);
+        }
+        System.out.println();
+        }
+    }
+}
