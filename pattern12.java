055521ebd8e5e1ad2d3fff891fdbd518e869ca5e
STROKE-MODS <strokemods@gmail.com>
2026-07-12 01:26:04 +0530

    Another commit:

---
+++ pattern12.java
@@ -0,0 +1,19 @@
+public class pattern12{
+    public static void main(String a[]){
+        int n=8;
+        for(int i=1;i<n;i++){
+            for(int k=0;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for(char hi='A';hi<'A'+i;hi++){
+                System.out.print(hi);
+            }
+            char r=(char)('A'+i-2);
+            for(int h=1;h<i;h++){
+                System.out.print(r);
+                r--;
+            }
+            System.out.println();
+        }
+    }
+}
