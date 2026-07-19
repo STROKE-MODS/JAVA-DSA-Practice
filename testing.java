c5c8fe1a798ce5e25f7c040354b93b16a383e136
STROKE-MODS <strokemods@gmail.com>
2026-07-20 00:20:06 +0530

    Changes done

--- testing.java
+++ testing.java
@@ -1,4 +1,11 @@
-import Java.util.Scanner
-class main(){
-    
+import java.util.Scanner;
+public class testing{
+    public static void main(String a[]){
+        Scanner scan = new Scanner(System.in);
+        int b=0;
+        System.out.print("Enter the number : ");
+        b = scan.nextInt();
+        System.out.print(b);
+        scan.close();
+    }
 }
